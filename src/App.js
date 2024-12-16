62717ad8202789dbbcdd1d7357c344c0d96e3c27
edwin <kamaueddiemaina@gmail.com>
2024-12-17 00:20:41 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginSignup from './loginSignup/LoginSignup';
+ // Adjust the path as necessary
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
